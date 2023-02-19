c3bce67644bfbaef970f836ae78b33785e1c8d3c
shweta-kadam562 <shwetakadam562@gmail.com>
2023-02-19 20:22:21 +0530

    AddRestaurant

--- src/components/restaurant-components/RestaurantList.js
+++ src/components/restaurant-components/RestaurantList.js
@@ -13,54 +13,54 @@ export class RestaurantList extends Component {
     this.props.listRestaurant();
   }
 
-  render() {
-    return (
-      <table className="table">
-        <thead>
-          {/* <tr> */}
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-          {/* </tr> */}
-        </thead>
-        <tbody>
-          {this.props.restaurantList.list.map((e, index) => (
-            <tr key={e.id}>
-              <th scope="row">{index + 1}</th>
-              <td>{e.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
-
-
-// render(){
-//     return(
-//         <table className="table">
+//   render() {
+//     return (
+//       <table className="table">
 //         <thead>
-//           <tr>
+//           {/* <tr> */}
 //             <th scope="col">#</th>
-//             <th scope="col">Restaurant Name</th>
-            
-//           </tr>
+//             <th scope="col">Name</th>
+//           {/* </tr> */}
 //         </thead>
 //         <tbody>
-//           {
-//             this.props.restaurantList.list.map((r, index) => (
-             
-//               <tr key={r.id}>
-//                 <th scope="row" key={r.id}> {index + 1}</th>
-//                 <td>{r.name}</td>
-//               </tr>
-            
-//           ))}   
+//           {this.props.restaurantList.list.map((e, index) => (
+//             <tr key={e.id}>
+//               <th scope="row">{index + 1}</th>
+//               <td>{e.name}</td>
+//             </tr>
+//           ))}
 //         </tbody>
 //       </table>
-//       )
+//     );
 //   }
 // }
+
+
+render(){
+    return(
+        <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Restaurant Name</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.props.restaurantList.list.map((r, index) => (
+             
+              <tr key={r.id}>
+                <th scope="row" key={r.id}> {index + 1}</th>
+                <td>{r.name}</td>
+              </tr>
+            
+          ))}   
+        </tbody>
+      </table>
+      )
+  }
+}
 
 function mapStateToProps(state) {
   return {
